e83f91a37483c82caf4c61721bfa688223cd657b
Kajal222 <kajal.singhai@mindcrewtech.com>
2022-09-26 16:07:47 +0530

    ass

--- src/layouts/backend/layout1.js
+++ src/layouts/backend/layout1.js
@@ -20,7 +20,7 @@ const Layout1 = () => {
             <HeaderStyle1 />
             
             <div className="content-page" id="content-page">
-                <Layout1Route />
+                <Layout1Route />aaaaaaaaaaaaaaaaaaaaa
             </div>
          </div>
          <FooterStyle />
