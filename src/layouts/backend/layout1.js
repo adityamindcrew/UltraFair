12d52ed99c734ad5e0c5d620b0e24cc4a00139cc
Madhurimindcrew <madhuri.mindcrew@gmail.com>
2022-10-21 16:54:29 +0530

    10/21/2022 {4:53}

--- src/layouts/backend/layout1.js
+++ src/layouts/backend/layout1.js
@@ -12,39 +12,43 @@ import Chat from '../../components/Chat/chat';
 import chat from '../../assets/images/Ultrafair/chat.svg'
 import search from '../../assets/images/Ultrafair/search.svg'
 import toggle from '../../assets/images/Ultrafair/toggle.svg'
+import { Link } from 'react-router-dom'
+import { useState } from 'react';
 
 const Layout1 = () => {
+    const [showchat, setShowchat] = useState(false);
     const minisidbar = () => {
         document.body.classList.toggle("sidebar-main");
-      };
-    
-    return(
+    };
+
+    return (
         <>
-         <div className="wrapper">
-            <SidebarStyle />
-            <HeaderStyle1 />
-            <div className="content-page" id="content-page">
-                <Layout1Route />
+            <div className="wrapper">
+                <SidebarStyle />
+                <HeaderStyle1 />
+                <div className="content-page" id="content-page">
+                    <Layout1Route />
+                </div>
             </div>
-         </div>
-         <FooterStyle />
-                    <nav class="navigation-bar" style={{ positon : 'sticky' , zIndex :99}}>
-                        <div className='d-flex maindiv justify-content-evenly'>
-                            <div className='m-2' onClick={minisidbar}>
-                                <img src={toggle} />
-                             <div> Menu</div>
-                            </div>
-                            <div className='m-2 '>
-                            <img src={search} />
-                            <div>
+            {showchat ? '' : <FooterStyle />}
+            <nav class="navigation-bar" style={{ positon: 'sticky', zIndex: 99 }}>
+                <div className='d-flex maindiv justify-content-evenly'>
+                    <div className='m-2' onClick={minisidbar}>
+                        <img src={toggle} />
+                        <div> Menu</div>
+                    </div>
+                    <div className='m-2 '>
+                        <img src={search} />
+                        <div>
                             Search</div>
-                            </div>
-                            <div className='m-2' onclick = "fun()">
+                    </div>
+                    <div className='m-2'>
+                        <Link to="/chat" onClick={setShowchat(!showchat)}>
                             <img src={chat} />
-                             <div>
-                               Chat</div>
-                            </div></div>
-                    </nav>
+                            <div style={{ color: "#D1D0CF" }}>
+                                Chat</div></Link>
+                    </div></div>
+            </nav>
         </>
     )
 }
