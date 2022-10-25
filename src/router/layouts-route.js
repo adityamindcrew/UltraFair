2ca779e58c773b1a3c42ae5f6f4a04197c5faf61
adtiyathakkar <78215658+adtiyathakkar@users.noreply.github.com>
2022-10-25 13:47:26 +0530

    new commit for frame modal

--- src/router/layouts-route.js
+++ src/router/layouts-route.js
@@ -1,11 +1,6 @@
 import {Switch,Route} from 'react-router-dom'
 import Layout1 from '../layouts/backend/layout1'
 import BlankLayout from '../layouts/blanklayout';
-<<<<<<< Updated upstream
-=======
-// import { Widget, addResponseMessage } from 'react-chat-widget';
-// import 'react-chat-widget/lib/styles.css';
->>>>>>> Stashed changes
 
 const LayoutsRoute = props => {
     return (
