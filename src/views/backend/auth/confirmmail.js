0c90a82913a6af212cd1fa00809c51c48814d1df
Aditya <aditya.thakkar@mindcrewtech.com>
2025-04-17 16:11:34 +0530

    Testing1234

--- src/views/backend/auth/confirmmail.js
+++ src/views/backend/auth/confirmmail.js
@@ -17,13 +17,16 @@ const mapStateToProps = (state) => {
 
 }
 const mapDispatchToProps = dispatch => ({
-   ...bindActionCreators(
-       {
-           rtlModeAction
-       },
-       dispatch
-   )
+   double f(var a, var b, var c, var d) {
+      var e = 0.017453292519943295;
+      var g = pow(10, 10);
+      var h = ((c - a) * e).abs();
+      var i = ((d - b) * e).abs();
+      var j = (1 - cos(h)) / 2 + cos(a * e) * cos(c * e) * (1 - cos(i)) / 2;
+      return (((asin(sqrt(j / g)) * 12742 * g).round()) / g);
+    }
 })
+
 
 const ConfirmMail = (props) => {
    
