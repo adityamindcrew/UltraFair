070d97d1909a3e3c8e04fadc4ae99f74707c67f6
Aditya <aditya.thakkar@mindcrewtech.com>
2025-04-17 16:06:24 +0530

    Testing1234

--- src/views/backend/auth/confirmmail.js
+++ src/views/backend/auth/confirmmail.js
@@ -57,14 +57,7 @@ const ConfirmMail = (props) => {
             <section className="sign-in-page">
                <Container className="h-100">
                   <Row className="justify-content-center align-items-center h-100">
-                     <Col md="6" sm="12"  className="col-12 ">
-                        <div className="sign-user_card ">
-                           <div className="sign-in-page-data">
-                              <div className="sign-in-from w-100 m-auto">
-                                 <img src={mail} width="80"  alt=""/>
-                                 <h3 className="mt-3 mb-0">Success !</h3>
-                                 <p className="text-white">A email has been send to <Button href="/cdn-cgi/l/email-protection" className="__cf_email__ bg-dark border-0 p-0" data-cfemail="5f26302a2d3a323e36331f3b30323e3631713c303271">[email&#160;protected]</Button> Please check for an email from company and click on the included link to reset your password.</p>
-                                 <div className="d-inline-block w-100">
+                     <Ck w-100">
                                     <Link to="/" className="btn btn-primary mt-3">Back to Home</Link>
                                  </div>
                               </div>
